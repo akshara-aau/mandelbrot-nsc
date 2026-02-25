3a16331467e6079f19246f8b4b4d3435bf7985d3
akshara-aau <asathe25@student.aau.dk>
2026-02-25 20:36:10 +0100

    Add naive mandelbrot point iteration function

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,20 +1,26 @@
-"""
-Mandelbrot Set Generator
-Author : Akshara
-Course : Numerical Scientific Computing 2026
-"""
-def f(x):
-    """
-    Example function .
-    Parameters
-    ----------
-    x : float
-    Input value
-    Returns
-    -------
-    float
-    Output value
-    """
-    #my test comment
-# TODO : Implement the algorithm
-    pass
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+xmin, xmax = -2.0, 1.0
+ymin, ymax = -1.5, 1.5
+width, height = 100, 100  # small grid for testing
+
+x_vals = np.linspace(xmin, xmax, width)
+y_vals = np.linspace(ymin, ymax, height)
+
+def mandelbrot_point(c, max_iter=100):
+    z = 0
+    for n in range(max_iter):
+        z = z*z + c
+        if abs(z) > 2:
+            return n
+    return max_iter
+
+
+print(mandelbrot_point(0, max_iter=100))
+print(mandelbrot_point(2, max_iter=100))
+print(mandelbrot_point(-0.75 + 0.1j, max_iter=100))
+
+
+
