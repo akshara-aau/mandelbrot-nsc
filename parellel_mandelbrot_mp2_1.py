a9cf2f45a06fb253612096217eadd8b1e6dc2143
akshara-aau <asathe25@student.aau.dk>
2026-03-22 10:42:48 +0100

    Verify result matches serial

--- parellel_mandelbrot_mp2_1.py
+++ parellel_mandelbrot_mp2_1.py
@@ -29,11 +29,27 @@ def mandelbrot_chunk(row_start, row_end, N, x_min, x_max, y_min, y_max, max_iter
 def mandelbrot_serial(N, x_min, x_max, y_min, y_max, max_iter=100):
     return mandelbrot_chunk(0, N, N, x_min, x_max, y_min, y_max, max_iter)
 
+def worker(args):
+    return mandelbrot_chunk(*args)
+
+def mandelbrot_parallel(N, x_min, x_max, y_min, y_max, max_iter, n_workers):
+    chunks = []
+    rows_per_worker = N // n_workers
+    for i in range(n_workers):
+        row_start = i * rows_per_worker
+        row_end = (i + 1) * rows_per_worker if i < n_workers - 1 else N
+        chunks.append((row_start, row_end, N, x_min, x_max, y_min, y_max, max_iter))
+    with Pool(processes=n_workers) as pool:
+        parts = pool.map(worker, chunks)
+    return np.vstack(parts)
+
 if __name__ == '__main__':
     from mandelbrot_functions import mandelbrot_numba
     N = 512
-    result_new = mandelbrot_serial(N, -2, 1, -1.5, 1.5, 100)
+    result_serial = mandelbrot_serial(N, -2, 1, -1.5, 1.5, 100)
     result_old = mandelbrot_numba(-2, 1, -1.5, 1.5, N, N, 100)
-    print("Arrays equal:", np.array_equal(result_new, result_old))
-    if not np.array_equal(result_new, result_old):
-        print("Differences:", np.sum(result_new != result_old))
+    print("Serial vs L03 equal:", np.array_equal(result_serial, result_old))
+    
+    n_workers = 4
+    result_parallel = mandelbrot_parallel(N, -2, 1, -1.5, 1.5, 100, n_workers)
+    print("Parallel vs Serial equal:", np.array_equal(result_parallel, result_serial))
